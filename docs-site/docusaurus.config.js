b81e70ae781e1d60e50dc45960a44c827dd88dd4
Lombardimn <matiiaslombardi@gmail.com>
2025-02-05 01:36:56 -0300

    fix(docs): modificación de url en docusaurus

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/Lombardimn.github.io',
+  url: 'https://Lombardimn.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/project-pitchSide/',
